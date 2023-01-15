3af806c7e34decfb250ec479f685c528040f08df
Rachel Collins <spiritedndiligent@gmail.com>
2023-01-14 21:25:10 -0500

    first buttons

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,26 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <h1>Geo-Quiz!</h1>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <button>North America</button>
+      <button>Africa</button>
+      <button>Europe</button>
+      <button>South America</button>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+}
+
+button {
+padding: 10px;
+width: 100px;
+background-color: teal;
 }
 
 .logo {

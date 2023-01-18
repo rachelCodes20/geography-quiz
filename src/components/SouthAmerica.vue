dfeff23212bd48b2d027a4d48eb708a399601b0b
Rachel Collins <spiritedndiligent@gmail.com>
2023-01-17 22:05:27 -0500

    add SA component

--- src/components/SouthAmerica.vue
+++ src/components/SouthAmerica.vue
@@ -1,1 +1,25 @@
-SAQuestion
+<script>
+  export default {
+  data() {
+	  return {
+    	SAQuestion1: 'What is the Northern most country in South America?'
+  	}
+  }
+},
+ 
+    created(){
+      //On Component created
+    },
+ 
+    methods: {
+      functionExample(){
+        //Basic Component method
+      }
+    }
+  }
+</script>
+<template>
+  <div id='SA'>
+   {{SAQuestion1}}
+  </div>
+</template>
